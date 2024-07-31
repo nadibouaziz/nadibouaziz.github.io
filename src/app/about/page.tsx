b0ca88212e5349a05a4ff095d0932d9fc4fc8542
nadibouaziz <nadibouaziz@fake-email.com>
2024-08-01 02:18:56 +0800

    fix EOL github

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <div className="main">About Page</div>
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return  <div className="main">Blog Page</div>
+}
