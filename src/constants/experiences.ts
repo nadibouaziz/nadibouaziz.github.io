9206ee266d0297fa17dc7b4b64d2fc709f5702a9
nadibouaziz <nadibouaziz@fake-email.com>
2024-08-07 12:59:47 +0800

    add experience component

---
+++ src/constants/experiences.ts
@@ -0,0 +1,50 @@
+export const experiences = [
+  {
+    title: "Senior Full Stack Developer",
+    company_name: "Lidl",
+    icon: "/assets/lidl.png",
+    iconBg: "#383E56",
+    date: "Jun 2022 - Jun 2023",
+    points: [
+      "Developed a notification service using NestJS, Kafka, Zookeeper and related technologies, enabling real-time price negotiations with warehouses.",
+      "Engaged in bi-weekly sprints, contributing to sprint planning, reviews, and retrospectives to enhance team performance and product quality",
+      "Developed a custom Transporter in NestJS for the Stomp protocol, connected with ActiveMQ."
+    ],
+  },
+  {
+    title: "Senior Backend Developer",
+    company_name: "Decathlon",
+    icon: "/assets/decathlon.png",
+    iconBg: "#E6DEDD",
+    date: "Aug 2019 - June 2022",
+    points: [
+      "Developed a Typescript application using ExpressJS, to consolidate supply chain information, with an 80% unit test coverage with Jest",
+      "Conducted code reviews and pair programming sessions to mentor junior developers, fostering their skills in coding best practices and improving overall code quality.",
+      "Developed and maintained Terraform configurations to deploy and manage AWS/GCP resources, including VMs, networks, and security groups.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "iTiTones",
+    icon: "/assets/ititones.png",
+    iconBg: "#383E56",
+    date: "Feb 2019 - May 2019",
+    points: [
+      "Developed a scalable REST API in NodeJS/Express, implementing unit tests with Mocha for robust functionality.",
+      "Implemented Firebase for user authentication, ensuring a secure and user-friendly login experience.",
+    ],
+  },
+  {
+    title: "APAC Lead : FullStack Developer",
+    company_name: "Caudalie",
+    icon: "/assets/caudalie.png",
+    iconBg: "#E6DEDD",
+    date: "May 2016 - Aug 2018",
+    points: [
+      "Developing and maintaining web applications using Magento ( PHP ) and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Orchestrated AWS architecture to forecast and handle peak server loads during high-traffic events like China's 11/11, ensuring optimal website performance and customer satisfaction."      
+    ],
+  },
+];
+
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,19 @@
+export const navLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+  {
+    title: "Resume",
+    path: "/resume.pdf",
+    className: "resume",
+  },
+];
