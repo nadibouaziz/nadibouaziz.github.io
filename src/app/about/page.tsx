770ac806bb3918def1e57cc21a5135cebdcd78ff
nadibouaziz <nadibouaziz@fake-email.com>
2024-08-07 12:58:20 +0800

    add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
-import About from "@/components/about/About";
+import About from "@/components/profile/Profile";
 import Experience from "@/components/experiences/Experience";
+import Skills from "@/components/skills/Skills";
 
 export default function Page() {
   return (
     <div className="main">
       <About />
       <Experience />
+      <Skills />
     </div>
   );
 }
